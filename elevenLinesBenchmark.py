dda6c43a633cab515b8263b869c26a455c3c3d22
Alex Merritt <lecaran@gmail.com>
2020-10-31 13:56:59 -0300

    fix numpy benchmark

--- elevenLinesBenchmark.py
+++ elevenLinesBenchmark.py
@@ -4,14 +4,14 @@ import time
 X = np.random.random((10000,10000)).astype('float32') 
 y = np.random.random((1,10000)).T.astype('float32')
 
-syn0 = 2*np.random.random((10000,10000)).astype('float32') - 1
-syn1 = 2*np.random.random((10000,1)).astype('float32') - 1
-
 print(X.shape)
 print(X.dtype)
 iter = 5
 
 def elevenlines(): 
+    syn0 = 2*np.random.random((10000,10000)).astype('float32') - 1
+    syn1 = 2*np.random.random((10000,1)).astype('float32') - 1
+    tic = time.time()
     for j in range(iter): 
         l1 = 1/(1+np.exp(-(np.dot(X,syn0))))  
         l2 = 1/(1+np.exp(-(np.dot(l1,syn1)))) 
@@ -19,10 +19,7 @@ def elevenlines():
         l1_delta = l2_delta.dot(syn1.T) * (l1 * (1-l1))
         syn1 += l1.T.dot(l2_delta)
         syn0 += X.T.dot(l1_delta)
+    toc = time.time()
+    print(toc-tic)
 
-tic = time.time()
 results = elevenlines() 
-toc = time.time()
-ret = (toc - tic)
-print(ret)
-
