1f30a8891674808ce4bab4ef64030bbc76faab8d
Alex Merritt <lecaran@gmail.com>
2022-10-31 20:11:58 -0400

    Remove redundant union type implementation

--- core/src/main/scala/ndscala/package.scala
+++ core/src/main/scala/ndscala/package.scala
@@ -1,13 +1,8 @@
 package org.sciscala
 
-import org.sciscala.ndscala.union._
-
 package object ndscala
 {
 //TODO: Add sigmoid, relu, etc.
   type AllSupported = Int | Long | Float | Double | Boolean
-  type AllUnionSupported = Union[Int]#or[Long]#or[Float]#or[Double]#or[Boolean]#create
-  type AllIsSupported[T] = Contains[T, AllUnionSupported] 
-
 }
 
